08ed73f67705ce327ac2c2101c11e471c731f050
Hao Wu <wuhao.wise@gmail.com>
2014-08-20 23:35:56 -0700

    add macosx build

--- build.sh
+++ build.sh
@@ -11,16 +11,46 @@ prepare() {
     cd ..
 }
 
+build_mac_one() {
+    SDK=macosx;
+    ARCH=$1;
+    MIN_VERSION="-mmacosx-version-min=10.7"
+    SDK_PATH=`xcrun -sdk $SDK --show-sdk-path`
+    APR_HOME=$SDK_PATH/usr
+    EXT_INCLUDE="$SDK_PATH/usr/include/apr-1/"
+
+    export CC="xcrun --sdk $SDK clang -arch $ARCH $MIN_VERSION"
+    export CPP="xcrun --sdk $SDK clang -E -arch $ARCH $MIN_VERSION"
+    export CFLAGS="-std=c99 -Ofast -I$EXT_INCLUDE" 
+    export LDFLAGS="-stdlib=libc++ -flto"
+
+    export CXX="xcrun --sdk $SDK clang++ -arch $ARCH $MIN_VERSION"
+    export CXXCPP="xcrun --sdk $SDK clang++ -E -arch $ARCH -std=c++11 $MIN_VERSION"
+    export CXXFLAGS="-Ofast -stdlib=libc++ -std=c++11 -Wno-c++11-narrowing -I$EXT_INCLUDE" 
+    
+    cd log4cxx
+    ./configure --prefix=$INSTALL_PREFIX$ARCH/log4cxx \
+        -enable-static -enable-shared \
+        --with-apr=$APR_HOME --with-apr-util=$APR_HOME
+    make clean && make -j8 && make install;
+    cd ..;
+}
+
 build_one() {
     SDK=$1;
     ARCH=$2;
-    export CC="xcrun --sdk $SDK clang -arch $ARCH -mios-version-min=5.0" 
-    export CPP="xcrun --sdk $SDK clang -E -arch $ARCH -mios-version-min=5.0"
+    MIN_VERSION=-mios-version-min=5.0
+
+    echo "Build for"
+    echo "SDK: $SDK, ARCH: $ARCH"
+    echo "SYSTEM: $MIN_VERSION"
+    export CC="xcrun --sdk $SDK clang -arch $ARCH $MIN_VERSION"
+    export CPP="xcrun --sdk $SDK clang -E -arch $ARCH $MIN_VERSION"
     export CFLAGS="-std=c99 -Ofast" 
     export LDFLAGS="-flto"
 
-    export CXX="xcrun -sdk $SDK clang++ -arch $ARCH -mios-version-min=5.0" 
-    export CXXCPP="xcrun -sdk $SDK clang++ -E -arch $ARCH -std=c++11 -mios-version-min=5.0" 
+    export CXX="xcrun -sdk $SDK clang++ -arch $ARCH $MIN_VERSION"
+    export CXXCPP="xcrun -sdk $SDK clang++ -E -arch $ARCH -std=c++11 $MIN_VERSION"
     export CXXFLAGS="-Ofast -stdlib=libc++ -std=c++11 -Wno-c++11-narrowing" 
     
     echo "Build apr1.5 ..."
@@ -60,25 +90,36 @@ build_one() {
 lipo_one() {
     LIB=$1
     ALL=$(find $INSTALL_PREFIX* -name $LIB);
+    if [ -z "$ALL" ]; then return -1; fi
     lipo -create $ALL -o $LIB
 }
 
 bundle() {
-    rm -rf output log4cxx.zip
-    mkdir -p output/lib
-    for each in libapr-1.a libaprutil-1.a libexpat.a liblog4cxx.a; do
-        lipo_one $each;
-        mv $each output/lib
+    SDK=$1
+    OUTPUT=$SDK-output
+    rm -rf $OUTPUT log4cxx-$SDK.zip
+    mkdir -p $OUTPUT/lib
+    for each in libapr-1.a libaprutil-1.a libexpat.a liblog4cxx.a liblog4cxx.dylib; do
+        if lipo_one $each; then
+            mv $each $OUTPUT/lib
+        fi
     done
-    cp -r ${INSTALL_PREFIX}arm64/log4cxx/include output/.
-    cd output;
-    zip -r log4cxx.zip *
-    mv log4cxx.zip ..
+    cp -r ${INSTALL_PREFIX}x86_64/log4cxx/include $OUTPUT/.
+    cd $OUTPUT;
+    zip -r log4cxx-$SDK.zip *
+    mv log4cxx-$SDK.zip ..
     cd ..
 }
 
-prepare
+#prepare
 
+rm -rf ${INSTALL_PREFIX}*
+build_mac_one x86_64
+build_mac_one i386
+
+bundle macosx
+
+rm -rf ${INSTALL_PREFIX}*
 build_one iphonesimulator x86_64
 build_one iphonesimulator i386
 
@@ -86,4 +127,4 @@ build_one iphoneos armv7
 build_one iphoneos armv7s
 build_one iphoneos arm64
 
-bundle
+bundle ios
